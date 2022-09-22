83d2b2dcc242505922d9547e554396b3d0b556ca
Ernő Tamás Bakos <ebakos@geomant.com>
2022-09-22 22:06:57 +0200

    fix host

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def index():
     return "It works"
 
 def run():
-    bottle.run(app=app, server = 'waitress')
+    bottle.run(app=app, server = 'waitress', host="0.0.0.0", port=80)
 
 if __name__ == "__main__":
     run()
